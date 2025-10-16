d44ccfcfc230983f25ee873c6b37a4f37f8f5349
WolfQuery <qwertman783@outlook.com>
2025-10-16 20:49:08 +0200

    added a bunch of user configs

--- configuration.nix
+++ configuration.nix
@@ -22,6 +22,30 @@ networking.hostName = "kronos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
+  # Enable bluetooth
+  hardware.bluetooth = {
+  enable = true;
+  powerOnBoot = true;
+  settings = {
+    General = {
+      # Shows battery charge of connected devices on supported
+      # Bluetooth adapters. Defaults to 'false'.
+      Experimental = true;
+      # When enabled other devices can connect faster to us, however
+      # the tradeoff is increased power consumption. Defaults to
+      # 'false'.
+      FastConnectable = false;
+    };
+    Policy = {
+      # Enable all controllers when they are found. This includes
+      # adapters present on start as well as adapters that are plugged
+      # in later on. Defaults to 'true'.
+      AutoEnable = true;
+    };
+  };
+};
+
+
   # Set your time zone.
   time.timeZone = "Europe/Prague";
 
@@ -60,6 +84,8 @@ networking.hostName = "kronos"; # Define your hostname.
   # Configure keymap in X11
    services.xserver.xkb.layout = "cz";
    services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  
 
   # Enable CUPS to print documents.
    services.printing.enable = true;
@@ -117,18 +143,31 @@ picom
 rofi
 feh
 clipman
-fira-code
-noto-fonts
-noto-fonts-emoji
 tree git lazygit killall acpi wirelesstools brightnessctl
     fastfetch auto-cpufreq btop virtualbox
 
     cargo
     rustc
     clippy
+    xclip
+
+    texstudio
+    texlive.combined.scheme-full
+    godotPackages_4_5.godot
+
 
    ];
 nixpkgs.config.allowUnfree = true;
+
+fonts = {
+  enableDefaultPackages = true;
+  packages = with pkgs; [
+	fira-code
+	noto-fonts
+	noto-fonts-emoji
+  ];
+};
+
 home-manager.users.mun = {
 	home.stateVersion = "25.05";
 
@@ -177,6 +216,7 @@ home-manager.users.mun = {
     telescope-nvim
     nvim-treesitter
     vim-commentary
+    neogit
   ];
 
   # Vimscript settings (run early)
@@ -204,8 +244,306 @@ home-manager.users.mun = {
     })
     vim.cmd.colorscheme "catppuccin"
 
+   vim.g.mapleader = " "
+
+   vim.api.nvim_set_keymap('n', '<leader>cd', ':NERDTreeCWD<CR>', { noremap = true, silent = true })
+
+
     require("telescope").setup{}
+
+    -- Normal mode tab navigation
+vim.keymap.set('n', 'gt', ':tabnext<CR>', { noremap = true, silent = true })     -- next tab
+vim.keymap.set('n', 'gT', ':tabprevious<CR>', { noremap = true, silent = true }) -- previous tab
+
+-- Open / close tabs
+vim.keymap.set('n', '<leader>tn', ':tabnew<CR>', { noremap = true, silent = true }) --open a new tab
+vim.keymap.set('n', '<leader>tc', ':tabclose<CR>', { noremap = true, silent = true }) -- close the current tab
+
   '';
+};
+
+programs.kitty = {
+enable = true;
+
+extraConfig = ''
+# BEGIN_KITTY_THEME
+
+# I prefer Maple Mono, but you can use a different font if you prefer.
+# Font, my favorite font is maple mono :)
+font_family Fira Code
+bold_font Fira Code Bold
+italic_font Fira Code Light
+
+# Make the cursor shape a beam
+shell_integration no-cursor
+cursor_shape beam
+
+# Padding
+window_padding_width 7
+window_padding_height 10
+
+# Scroll back up to 3000 lines.
+scrollback_lines 3000
+
+#Default font size.
+font_size 11
+
+# Just a keybind to change font size to your liking, it's CTRL + scroll wheel up or down.
+map ctrl+shift+plus change_font_size all +1.0
+map ctrl+shift+minus change font size all -1.0
+
+# Background opacity, set to 0.7.
+# Blur works with hyprland, or sway-fx as a drop-in replacement for sway.
+background_opacity 0.7
+
+
+# Finally, the COLORS! these use the design system I made for this rice.
+cursor               #AC82E9
+
+selection_background #d8cab8
+selection_foreground #141216
+
+background           #141216
+foreground           #d8cab8
+
+color0               #2b2135
+color8               #92fcfa
+color1               #fc4649
+color9               #fc4649
+color2               #c4e881
+color10              #c4e881
+color3               #AC82E9
+color11              #AC82E9
+color4               #7b91fc
+color12              #7b91fc
+color5               #f3fc7b
+color13              #f3fc7b
+color6               #8F56E1
+color14              #8F56E1
+color7               #fc92fc
+color15              #d8cab8
+
+
+# END_KITTY_THEME
+'';
+};
+
+services.polybar = {
+	enable = true;
+	script = "polybar example;";
+	config = ''
+		;==========================================================
+;
+;
+;   ██████╗  ██████╗ ██╗  ██╗   ██╗██████╗  █████╗ ██████╗
+;   ██╔══██╗██╔═══██╗██║  ╚██╗ ██╔╝██╔══██╗██╔══██╗██╔══██╗
+;   ██████╔╝██║   ██║██║   ╚████╔╝ ██████╔╝███████║██████╔╝
+;   ██╔═══╝ ██║   ██║██║    ╚██╔╝  ██╔══██╗██╔══██║██╔══██╗
+;   ██║     ╚██████╔╝███████╗██║   ██████╔╝██║  ██║██║  ██║
+;   ╚═╝      ╚═════╝ ╚══════╝╚═╝   ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝
+;
+;
+;   To learn more about how to configure Polybar
+;   go to https://github.com/polybar/polybar
+;
+;   The README contains a lot of information
+;
+;==========================================================
+
+[colors]
+background = #1c182d
+background-alt = #2b1b3d
+foreground = #d0b6fd
+primary = #cfb5fd
+secondary = #8a78b0
+alert = #7b91fc
+disabled = #707880
+
+[bar/example]
+width = 100%
+height = 30pt
+radius = 15
+
+; dpi = 96
+
+background = "$${colors.background}"
+foreground = $${colors.foreground}
+
+line-size = 4pt
+
+border-size = Opt
+border-color = #00000000
+
+padding-left = 0
+padding-right = 1
+
+module-margin = 1
+
+separator = |
+separator-foreground = $${colors.disabled}
+
+font-0 = monospace;2
+
+modules-left = xworkspaces xwindow
+modules-right = filesystem memory cpu wlan xkeyboard battery date
+
+cursor-click = pointer
+cursor-scroll = ns-resize
+
+enable-ipc = true
+
+; wm-restack = generic
+; wm-restack = bspwm
+; wm-restack = i3
+
+; override-redirect = true
+
+; This module is not active by default (to enable it, add it to one of the
+; modules-* list above).
+; Please note that only a single tray can exist at any time. If you launch
+; multiple bars with this module, only a single one will show it, the others
+; will produce a warning. Which bar gets the module is timing dependent and can
+; be quite random.
+; For more information, see the documentation page for this module:
+; https://polybar.readthedocs.io/en/stable/user/modules/tray.html
+[module/systray]
+type = internal/tray
+
+format-margin = 8pt
+tray-spacing = 16pt
+
+[module/xworkspaces]
+type = internal/xworkspaces
+
+label-active = %name%
+label-active-background = $${colors.background-alt}
+label-active-underline= $${colors.primary}
+label-active-padding = 1
+
+label-occupied = %name%
+label-occupied-padding = 1
+
+label-urgent = %name%
+label-urgent-background = $${colors.alert}
+label-urgent-padding = 1
+
+label-empty = %name%
+label-empty-foreground = $${colors.disabled}
+label-empty-padding = 1
+
+[module/xwindow]
+type = internal/xwindow
+label = %title:0:60:...%
+
+[module/filesystem]
+type = internal/fs
+interval = 25
+
+mount-0 = /
+
+label-mounted = %{F#F0C674}%mountpoint%%{F-} %percentage_used%%
+
+label-unmounted = %mountpoint% not mounted
+label-unmounted-foreground = $${colors.disabled}
+
+[module/pulseaudio]
+type = internal/pulseaudio
+
+format-volume-prefix = "VOL "
+format-volume-prefix-foreground = $${colors.primary}
+format-volume = <label-volume>
+
+label-volume = %percentage%%
+
+label-muted = muted
+label-muted-foreground = $${colors.disabled}
+
+[module/xkeyboard]
+type = internal/xkeyboard
+blacklist-0 = num lock
+
+label-layout = %layout%
+label-layout-foreground = $${colors.primary}
+
+label-indicator-padding = 2
+label-indicator-margin = 1
+label-indicator-foreground = $${colors.background}
+label-indicator-background = $${colors.secondary}
+
+[module/memory]
+type = internal/memory
+interval = 2
+format-prefix = "RAM "
+format-prefix-foreground = $${colors.primary}
+label = %percentage_used:2%%
+
+[module/cpu]
+type = internal/cpu
+interval = 2
+format-prefix = "CPU "
+format-prefix-foreground = $${colors.primary}
+label = %percentage:2%%
+
+[network-base]
+type = internal/network
+interval = 5
+format-connected = <label-connected>
+format-disconnected = <label-disconnected>
+label-disconnected = %{F#F0C674}%ifname%%{F#707880} disconnected
+
+[module/wlan]
+inherit = network-base
+interface-type = wireless
+label-connected = %{F#F0C674}%ifname%%{F-} %essid%
+
+[module/eth]
+inherit = network-base
+interface-type = wired
+label-connected = %{F#F0C674}%ifname%%{F-}
+
+[module/date]
+type = internal/date
+interval = 1
+
+date = %H:%M
+date-alt = %Y-%m-%d %H:%M:%S
+
+label = %date%
+label-foreground = $${colors.primary}
+
+[settings]
+screenchange-reload = true
+pseudo-transparency = true
+
+[module/battery]
+type = internal/battery
+
+; This is useful in case the battery never reports 100% charge
+; Default: 100
+full-at = 100
+
+; format-low once this charge percentage is reached
+; Default: 10
+; New in version 3.6.0
+low-at = 15
+
+; Use the following command to list batteries and adapters:
+; $ ls -1 /sys/class/power_supply/
+battery = BAT0
+adapter = ADP1
+
+; If an inotify event haven't been reported in this many
+; seconds, manually poll for new values.
+;
+; Needed as a fallback for systems that don't report events
+; on sysfs/procfs.
+;
+; Disable polling by setting the interval to 0.
+;
+; Default: 5
+poll-interval = 5
+; vim:ft=dosini
+	'';
 };
 
 
@@ -220,7 +558,7 @@ home-manager.users.mun = {
 
   # List services that you want to enable:
 
-  # Enable the OpenSSH daemon.
+  # Enable the OpenS/pipeSH daemon.
    services.openssh.enable = true;
 
   # Open ports in the firewall.
